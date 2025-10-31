05411287e8434bb585dd202c0a3a6970b4b9443c
Arturo <lemosart0987@gmail.com>
2025-10-31 17:50:02 -0500

    First commit, created db fullstack_employees and added create table employees in schema.sql.

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,11 @@
 DROP TABLE IF EXISTS employees;
 
+-- TO DO
+
+CREATE TABLE employees (
+    id serial PRIMARY KEY,
+    name text UNIQUE NOT NULL,
+    birthday date NOT NULL,
+    salary integer NOT NULL
+)
 
